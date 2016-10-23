c052998c15891c13e3135e12fb37abd027384e34
Conor Hall <concon8wb@gmail.com>
2016-10-23 15:38:19 +0100

    Find Multiple Quitars
    Changed search method in Inventory to show multiple matching guitars
    instead of one.

--- src/Inventory.java
+++ src/Inventory.java
@@ -25,7 +25,8 @@ public class Inventory {
     }
     return null;
   }
-  public Guitar search(Guitar searchGuitar) {
+  public List search(Guitar searchGuitar) {
+    List matchingGuitars = new LinkedList();
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
       // Ignore serial number since that's unique
@@ -42,8 +43,8 @@ public class Inventory {
         continue;
       if (searchGuitar.getTopWood() != guitar.getTopWood())
         continue;
-      return guitar;
+      matchingGuitars.add(guitar);
     }
-    return null;
+    return matchingGuitars;
   }
 }
